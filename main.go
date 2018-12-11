f6317055b8c4798ea14b03afc60424d61d784b39
Trim Bresilla <trim.bresilla@gmail.com>
2018-12-11 07:59:45 +0100

    using urfave.cli.v1

--- main.go
+++ main.go
@@ -1,1 +1,24 @@
 package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+
+	cli "gopkg.in/urfave/cli.v1"
+)
+
+func main() {
+	app := cli.NewApp()
+	app.Name = "Note"
+	app.Usage = "A notetaking app from your CLI"
+	app.Action = func(c *cli.Context) error {
+		fmt.Println("boom! I say!")
+		return nil
+	}
+
+	err := app.Run(os.Args)
+	if err != nil {
+		log.Fatal(err)
+	}
+}
