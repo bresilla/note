6f56f53888dfdfbf0b3659465eebb47c7bb39566
Trim Bresilla <trim.bresilla@gmail.com>
2018-12-12 17:27:19 +0100

    structural changes and add note fixes

--- src/note_add.go
+++ src/note_add.go
@@ -3,32 +3,30 @@ package note
 import (
 	"fmt"
 	"os"
+	"path"
 )
 
 func MakeNote(name string) {
-	num, exnote := FindNote(name)
+	folder, file := path.Split(name)
+	num, found_note := FindNote(file)
 	if num == -1 {
-		var newfile string = DataBase() + "/" + name + ".md"
-		file, err := os.Create(newfile)
-		ErrorCheck(err)
-		defer file.Close()
-		fmt.Printf("Note %q has been created\n", newfile)
+		if folder != "" {
+			joinDir := DataBase() + "/" + folder
+			os.MkdirAll(joinDir, os.ModePerm)
+			newfile := joinDir + file + ".md"
+			note, err := os.Create(newfile)
+			ErrorCheck(err)
+			defer note.Close()
+			fmt.Printf("Note %q has been created\n", newfile)
+		} else {
+			newfile := DataBase() + "/" + file + ".md"
+			note, err := os.Create(newfile)
+			ErrorCheck(err)
+			defer note.Close()
+			fmt.Printf("Note %q has been created\n", newfile)
+		}
 	} else {
 		fmt.Println("The note name name already exists:")
-		fmt.Println(num, exnote)
-	}
-}
-
-func MakeNoteTest(name string) {
-	num, exnote := FindNote(name)
-	if num == -1 {
-		var newfile string = DataBase() + "/" + name + ".md"
-		file, err := os.Create(newfile)
-		ErrorCheck(err)
-		defer file.Close()
-		fmt.Printf("Note %q has been created\n", newfile)
-	} else {
-		fmt.Println("The note name name already exists:")
-		fmt.Println(num, exnote)
+		fmt.Println(num, found_note)
 	}
 }
--- util_test.go
+++
@@ -1,9 +0,0 @@
-package main
-
-import (
-	"fmt"
-)
-
-func TestSnip() {
-	fmt.Println("TEST")
-}
